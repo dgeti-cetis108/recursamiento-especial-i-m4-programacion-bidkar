32edf3f3b2c1ef1cb86448044463ffc5a9cb8363
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2021-05-03 15:45:52 -0600

    Sesión 4 - 03/05/2021
    - Agregar el modelo User
    - Agregar el paquete EntityFramework Core SQLite

---
+++ UsersApp/Models/User.cs
@@ -0,0 +1,15 @@
+namespace UsersApp
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Photo { get; set; }
+        public string Status { get; set; }
+        public string RememberPassword { get; set; }
+    }
+}
